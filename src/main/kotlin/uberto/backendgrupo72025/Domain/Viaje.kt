b6d99b36dd313d8243399795dfcd4f370cf6f072
gasabe <gabelardo@estudiantes.unsam.edu.ar>
2025-03-11 00:03:17 -0300

    agrego metodos y validacion de comentarios

--- src/main/kotlin/uberto/backendgrupo72025/Domain/Viaje.kt
+++ src/main/kotlin/uberto/backendgrupo72025/Domain/Viaje.kt
@@ -1,16 +1,18 @@
 package uberto.backendgrupo72025.Domain
 
 import Conductor
+import Viajero
 import java.time.LocalDateTime
 
 class Viaje(
-    val origen : String,
-    val destino : String,
-    val fecha : LocalDateTime,
-    val cantidadDePasajeros : Int,
-    var duracion : Int,
-    val chofer : Conductor
-    ) {
+    val origen: String,
+    val destino: String,
+    val fecha: LocalDateTime,
+    val cantidadDePasajeros: Int,
+    var duracion: Int,
+    val chofer: Conductor,
+    private val comentarios: MutableList<Comentario> = mutableListOf()
+) {
 
     fun duracionAleatoria() {
         duracion = (1..99).random()
@@ -19,4 +21,13 @@ class Viaje(
     fun costoDelViaje(): Double {
         return chofer.precioBaseDelViaje + duracion * chofer.vehiculo.tipoVehiculo.calculoExtra(this)
     }
+
+    fun getComentarios(): List<Comentario> = comentarios.toList()
+
+    fun agregarComentario(comentario: Comentario) {
+        if (!comentarioValido(comentario)) throw Exception("No se puede calificar")
+        comentarios.add(comentario)
+    }
+
+    private fun comentarioValido(comentario: Comentario): Boolean = comentario.autor is Viajero
 }

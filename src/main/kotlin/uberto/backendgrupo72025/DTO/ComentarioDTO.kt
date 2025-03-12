0e4af5719e7b25a815df32c9f5220d16ee7a1fc4
gasabe <gabelardo@estudiantes.unsam.edu.ar>
2025-03-11 21:17:10 -0300

    creaccion de dto comentario

---
+++ src/main/kotlin/uberto/backendgrupo72025/DTO/ComentarioDTO.kt
@@ -0,0 +1,17 @@
+package uberto.backendgrupo72025.DTO
+
+import uberto.backendgrupo72025.Domain.Comentario
+
+class ComentarioDTO(
+    val viajeId: Int,
+    val autor: String,
+    val puntaje: Int,
+    val mensaje: String
+) {
+    fun Comentario.toDTO() = ComentarioDTO(
+        viajeId = viajeId,
+        autor = autor.nombreYApellido,
+        puntaje = puntaje,
+        mensaje = mensaje,
+    )
+}

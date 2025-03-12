a13c82f69603c0e6a3fa447a42974f7c4fce8112
gasabe <gabelardo@estudiantes.unsam.edu.ar>
2025-03-11 22:39:47 -0300

    avance con repositorio generico

--- src/main/kotlin/uberto/backendgrupo72025/DTO/ComentarioDTO.kt
+++ src/main/kotlin/uberto/backendgrupo72025/DTO/ComentarioDTO.kt
@@ -3,12 +3,14 @@ package uberto.backendgrupo72025.DTO
 import uberto.backendgrupo72025.Domain.Comentario
 
 class ComentarioDTO(
+    val id: Int,
     val viajeId: Int,
     val autor: String,
     val puntaje: Int,
     val mensaje: String
 ) {
     fun Comentario.toDTO() = ComentarioDTO(
+        id = this.id,
         viajeId = viajeId,//falta crear el id del viaje para vincularlo
         autor = autor.nombreYApellido,
         puntaje = puntaje,

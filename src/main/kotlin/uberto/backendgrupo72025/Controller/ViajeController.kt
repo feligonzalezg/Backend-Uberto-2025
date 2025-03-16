fcae51a4444de1098e7cd36c0684741275dd396c
Juanignacioz <jzotelo@estudiantes.unsam.edu.ar>
2025-03-16 00:05:30 -0400

    se avanza controller de crear viaje

---
+++ src/main/kotlin/uberto/backendgrupo72025/Controller/ViajeController.kt
@@ -0,0 +1,23 @@
+package uberto.backendgrupo72025.Controller
+
+import io.swagger.v3.oas.annotations.Operation
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+import uberto.backendgrupo72025.DTO.ViajeDTO
+import uberto.backendgrupo72025.Service.ViajeService
+
+
+@RestController
+@CrossOrigin("*")
+class Viajeontroller (@Autowired val viajeService: ViajeService) {
+
+
+    @PostMapping
+    @Operation(summary = "crear viaje")
+    fun crearViaje(viajeDTO: ViajeDTO){
+
+    }
+
+}
---
+++ src/main/kotlin/uberto/backendgrupo72025/Service/ViajeService.kt
@@ -0,0 +1,13 @@
+package uberto.backendgrupo72025.Service
+
+import org.springframework.stereotype.Service
+import uberto.backendgrupo72025.DTO.ViajeDTO
+import uberto.backendgrupo72025.Repository.UsuarioRepository
+
+@Service
+class ViajeService(val viajeRepository: UsuarioRepository) {
+
+    fun create(viajeDto: ViajeDTO){
+
+    }
+}

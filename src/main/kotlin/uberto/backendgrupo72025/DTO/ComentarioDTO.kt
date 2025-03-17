d40605a6ea452cafe50deab968471fa5718d746d
feligonzalezg <felipeogonzalezg@hotmail.com>
2025-03-16 22:24:21 -0300

    Endpoint datos perfil terminado

--- src/main/kotlin/uberto/backendgrupo72025/DTO/ComentarioDTO.kt
+++ src/main/kotlin/uberto/backendgrupo72025/DTO/ComentarioDTO.kt
@@ -3,16 +3,16 @@ package uberto.backendgrupo72025.DTO
 import uberto.backendgrupo72025.Domain.Comentario
 import java.time.LocalDate
 
-class ComentarioDTO(
+data class ComentarioDTO(
     val autor: String,
     val puntaje: Int,
     val mensaje: String,
     val fecha: LocalDate
-) {
-    fun Comentario.toComentarioDTO() = ComentarioDTO(
-        autor = "$autor.nombre $autor.apellido",
-        puntaje = puntaje,
-        mensaje = mensaje,
-        fecha = fecha
-    )
-}
+)
+
+fun Comentario.toComentarioDTO() = ComentarioDTO(
+    autor = autor.nombre + " " + autor.apellido,
+    puntaje = puntaje,
+    mensaje = mensaje,
+    fecha = fecha
+)

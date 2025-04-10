cdcf1d5be96c89b63911048a0c1555db69bea71d
feligonzalezg <felipeogonzalezg@hotmail.com>
2025-04-10 16:25:57 -0300

    correccion

--- src/main/kotlin/uberto/backendgrupo72025/DTO/ConductorDTO.kt
+++ src/main/kotlin/uberto/backendgrupo72025/DTO/ConductorDTO.kt
@@ -5,7 +5,7 @@ import uberto.backendgrupo72025.Service.ComentarioService
 import kotlin.math.roundToInt
 
 data class ConductorDTO(
-    val idConductor: Long,
+    val id: Long,
     val nombreYApellido: String,
     val dominio: String,
     val modelo: String,
@@ -13,11 +13,12 @@ data class ConductorDTO(
     val importe: Double,
     val marca: String,
     val anio: Int,
-    val foto: String
+    val foto: String,
+    val esChofer: Boolean
 )
 
 fun Conductor.toConductorDTO(cantidadDePasajeros: Int, duracion: Int, calificacion: Double) = ConductorDTO(
-        idConductor = id,
+        id = id,
         nombreYApellido = nombreYApellido(),
         dominio = vehiculo.dominio,
         marca = vehiculo.marca,
@@ -25,5 +26,6 @@ fun Conductor.toConductorDTO(cantidadDePasajeros: Int, duracion: Int, calificaci
         anio = vehiculo.anio,
         calificacion = ((calificacion* 10.0).roundToInt() / 10.0),
         importe = importeViaje(cantidadDePasajeros, duracion),
-        foto = foto
+        foto = foto,
+        esChofer = esChofer
     )
